99611c23a5c6299a3cddd4098ad505a07d9d64b9
Veenstw <praveen.p.choudhary98@gmail.com>
2025-02-06 23:17:11 +0530

    Add keys part 2

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ const FACEBOOK_API_BASE_URL = `https://graph.facebook.com/${FACEBOOK_API_VERSION
 
 // Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  projectId: process.env.FIREBASE_PROJECT_ID
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+  }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 // Add timeout to avoid hanging requests
